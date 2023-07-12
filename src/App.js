909660f2c77e21da73a89cc971cdcd86e1b3ed27
carlT <carl.tallon@sap.com>
2023-07-12 14:35:10 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+SAP Calendar app Carl Tallon.
+All other points of reference are available in the README.md.
+*/
+
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+ 
+import Footer from "./components/footer";
+import Navbar from "./components/navbar";
+import Login from "./components/login";
+import Home from "./components/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id = "background">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </div> 
   );
 }
-
+  
 export default App;
