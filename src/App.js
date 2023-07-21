ac212fef24359a361645e189f4f05e90b59fa1e0
carlT <carl.tallon@sap.com>
2023-07-21 13:54:02 +0100

    package.json

--- src/App.js
+++ src/App.js
@@ -12,16 +12,18 @@ import Home from "./components/home";
 import Account from "./components/account";
 import Seat from "./components/seat";
 import Newevent from "./components/newevent";
+import Register from "./components/register";
 
 function App() {
   return (
-    <div class = "background">
+    <div id = "background" class = "background">
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/account" element={<Account />} />
         <Route path="/seat" element={<Seat />} />
         <Route path="/create" element={<Newevent />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
     </div>
