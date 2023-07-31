89ebe1ebb8984b1d077a5f44bce77650575afe49
carlT <carl.tallon@sap.com>
2023-07-31 21:21:43 +0100

    Logging of working from home/office buttons to DB, to be used on seating page. Half Nine on a monday night!!!!

--- src/components/location.js
+++ src/components/location.js
@@ -1,4 +1,8 @@
 import './location.css';
+import { initializeApp } from "firebase/app";
+import { getAuth  } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore"; 
 
 function location() {
   return (
@@ -7,9 +11,9 @@ function location() {
         <h2>Your Location today?</h2>
         <h4>Let your coworkers know where you are working from today!</h4>
 
-        <button class = 'locationbtnathome'></button>
+        <button onClick = {workingfromhome} class = 'locationbtnathome'></button>
 
-        <button class = 'locationbtnatoffice'></button>
+        <button onClick = {workingfromoffice} class = 'locationbtnatoffice'></button>
     </div>
     
 
@@ -17,3 +21,65 @@ function location() {
 }
 
 export default location;
+
+
+function workingfromhome() {
+
+  const firebaseConfig = {
+    // Your Firebase configuration here
+    apiKey: "AIzaSyCf8LYQfeurGdAvB5Uu_eeQIVoWyl6Z3IY",
+    authDomain: "test-30bf7.firebaseapp.com",
+    databaseURL: "https://test-30bf7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "test-30bf7",
+    storageBucket: "test-30bf7.appspot.com",
+    messagingSenderId: "381008086519",
+    appId: "1:381008086519:web:f5fce4c537e56933ca1af2",
+    measurementId: "G-PMRE4PWB4B"
+  };
+  
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  const { uid } = auth.currentUser;
+
+  const locationRef = addDoc(collection(db, "Locations"), {
+    location: "Home",
+    UsedID: uid,
+  });
+
+}
+
+function workingfromoffice() {
+
+  const firebaseConfig = {
+    // Your Firebase configuration here
+    apiKey: "AIzaSyCf8LYQfeurGdAvB5Uu_eeQIVoWyl6Z3IY",
+    authDomain: "test-30bf7.firebaseapp.com",
+    databaseURL: "https://test-30bf7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "test-30bf7",
+    storageBucket: "test-30bf7.appspot.com",
+    messagingSenderId: "381008086519",
+    appId: "1:381008086519:web:f5fce4c537e56933ca1af2",
+    measurementId: "G-PMRE4PWB4B"
+  };
+  
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+
+  const { uid } = auth.currentUser;
+
+  const locationRef = addDoc(collection(db, "Locations"), {
+    location: "Office",
+    UserID: uid,
+  });
+
+}
