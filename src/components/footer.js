f82b74096ea77e6f23dd0cf7d58e0ff0d3f3e890
carlT <carl.tallon@sap.com>
2023-08-18 13:04:03 +0100

    Small fixes

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,8 @@ import './footer.css';
 export default function footer() {
   return (
     <footer id = "footer">
-      <h4>Created by SAP Interns 2023</h4>
+      <h4>SAP 2023</h4>
+      <h4>For support contact: <a>interns@sap.com</a></h4>
     </footer>
   );
 }
